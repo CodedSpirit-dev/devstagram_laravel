0bf2ad0206293375bb4d9a23c557f2e4cc2e12cf
Victor Irad Verdeja Trujillo <VIradVT@gmail.com>
2024-10-18 08:26:58 -0600

    Creando las primeras partes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,14 +9,24 @@
     @Vite('resources/js/app.js')
 </head>
 <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-    <header>
-        <nav>
+    <header class="p-5 border-b border-gray-200 dark:border-gray-700  bg-zinc-900 text-white p-5">
+        <nav class="mb-5">
             <ul class="font-semibold">
                 <li><a href="{{ url('/') }}">Home</a></li>
                 <li><a href="{{ url('/about') }}">About</a></li>
                 <li><a href="{{ url('/contact') }}">Contact</a></li>
                 <li><a href="{{ url('/store') }}">Store</a></li>
             </ul>
+        </nav>
+        <div class="container mx-auto flex justify-between items-center">
+            <h1 class="text-3xl font-bold">
+                Devstagram
+            </h1>
+        </div>
+
+        <nav>
+            <a class="font-bold uppercase text-gray-300 text-sm" href="{{ url('/login') }}">Login</a>
+            <a class="font-bold uppercase text-gray-300 text-sm" href="{{ url('/register') }}">Register</a>
         </nav>
     </header>
 
@@ -31,6 +41,5 @@
         <p>&copy; {{ date('Y') }} My Application. All rights reserved.</p>
     </footer>
 
-    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
